7970f1544f3d49bb8bd989b7c54654ee521c48b4
kellyf22 <kelly.a.fry22@gmail.com>
2021-01-14 22:14:53 -0500

    cleaned up code and added comments

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -8,41 +8,37 @@ with open(csvpath) as csvfile:
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
 
-    #print(csvreader)
-
     # Read the header row first (skip this step if there is now header)
     csv_header = next(csvreader)
-    #print(f"CSV Header: {csv_header}")
 
     #make a python list out of our csv data 
     election_data_list = [row for row in csvreader]
-    #print(election_data_list)
-    #print(data_list[0])
-    #print(data_list[0][1])
 
-
-    #HW requirement #1: find total number of votes cast (which is the length of the list, which doesn't include the header)
+    #HW requirement #1: find total number of votes cast (which is the length of the list)
     num_votes = (len(election_data_list))
 
-    #make a dictionary of candidates to store their name and running vote total, as in:
-    #candidates = {'name1': number of votes,
-    #              'name2': number of votes
-    #               }
-    candidates = {}
-    
-    #HW requirement #2: find vote totals (absolute and percent) for each candidate
-    
+    # HW requirement #2: find vote totals (absolute and percent) for each candidate.
+    # Start by making a dictionary of candidates to store their name and running vote total, as in:
+    # candidates = {'name1': number of votes,
+    #               'name2': number of votes
+    #                }
+    candidates = {}    
     for row in election_data_list:
         name = row[2]
         
-        #if candidate's name is in our dictionary, update his/her vote total
+        # if the candidate's name is already in our dictionary, update their vote total
         if name in candidates: 
             candidates[name] = candidates[name]+1
             
-        #otherwise, add that candidate and their first vote to the dictionary   
+        # otherwise, add that candidate and their first vote to the dictionary   
         else:
             candidates[name] = 1
 
+# Now that we have totals for each candidate, find the winner.
+max_votes = max(candidates.values())
+winner = [name for name, number in candidates.items() if number == max_votes]
+
+# Write the results to the terminal.
 print("Election Results")
 print("-----------------------")
 print(f'Total votes: {num_votes}''')
@@ -50,12 +46,10 @@ print("-----------------------")
 for name, number in candidates.items():
     print(f'{name}: {round(100*number/num_votes,3)}% ({number})''')
 print("-----------------------")
-max_votes = max(candidates.values())
-winner = [name for name, number in candidates.items() if number == max_votes]
 print(f'Winner: {winner[0]}''')
 print("-----------------------")
 
-# Write the same results to a text file called "results.txt" and put it in the PyBank folder.
+# Write the same results to a text file called "results.txt" and put it in the PyPoll folder.
 # The text file will be created if it doesn't already exist. If it does exist, it will be overwritten.
 text_report = open("results.txt","w+")
 text_report.write("Election Results\r")
